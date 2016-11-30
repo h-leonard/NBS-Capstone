97ed210f4a910b7286010c1f90e8a1f10736acfc
Chris Patrick <ellemsee@gmail.com>
2016-11-30 10:59:01 -0500

    Add files via upload
    Initial file for use in installing packages

---
+++ initial_setup.R
@@ -0,0 +1,7 @@
+### Run this code before running any of the files. This should only need to be done a single time
+### on initial setup.
+
+install.packages("knitr")
+install.packages("markdown")
+install.packages("rmarkdown")
+install.packages("dplyr")
