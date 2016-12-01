2b30f79ad28e05bf2db3fd7cec7a935c87757eeb
Chris Patrick <ellemsee@gmail.com>
2016-12-01 11:01:06 -0500

    Add files via upload
    r file to automatically load packages needed to run hospital reports

---
+++ load_packages.R
@@ -0,0 +1,8 @@
+# Load packages for Newborn Screening Hospital Reporting
+# Do not access this file directly; "run_file_and_variable_setting.R"
+# will automatically run this file.
+
+library(knitr)
+library(markdown)
+library(rmarkdown)
+library(dplyr)
