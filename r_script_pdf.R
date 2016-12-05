fbf166e20989e5bb641cd2d7a51591f44fe4a7ea
Hampton Leonard <hll4ce@virginia.edu>
2016-12-05 12:42:41 -0500

    work with past quarters

--- r_script_pdf.R
+++ r_script_pdf.R
@@ -75,6 +75,27 @@ unsat11 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 11])))/nrow(dd)
 unsat12 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 12])))/nrow(dd)
 unsat13 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 13])))/nrow(dd)
 
+
+# Unsat samples per each hospital for previous 3 quarters
+
+quarter_start <- seq(as.Date(start_date, "%m/%d/%Y"), as.Date(end_date, "%m/%d/%Y"), by = "quarter")
+
+quarter_end <- c(as.Date(as.yearqtr(quarter_start[2])) - 1,
+                 as.Date(as.yearqtr(quarter_start[3])) - 1, as.Date(as.yearqtr(quarter_start[4])) - 1,
+                 as.Date(end_date, "%m/%d/%Y"))
+
+
+hospital_metrics1 <- dd
+hospital_metrics1sub <- hospital_metrics1[hospital_metrics1$BIRTHDATE >= quarter_start[2] & hospital_metrics1$BIRTHDATE <= quarter_end[2],]
+
+
+for (i in hospital_metrics1sub$SUBMITTERNAME) {
+  hospital_metrics1sub["unsat%"] <- hospital_metrics$unsat_count/hospital_metrics$total_samples
+}
+
+
+
+
 # Rank hospitals by mean transit time (ascending order; e.g., least mean transit time = #1)
 hospital_metrics$rank_transit <- rank(hospital_metrics$avg_transit_time[hospital_metrics$SUBMITTERNAME != "NA"], 
                                       na.last="keep", ties.method="min")
