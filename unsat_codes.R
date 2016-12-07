d9ae69d78645c0b19831f1bdef9300e71644d3de
Chris Patrick <ellemsee@gmail.com>
2016-12-07 17:09:57 -0500

    Adding unsat_codes file

---
+++ unsat_codes.R
@@ -0,0 +1,15 @@
+# Codes for unsatisfactory results
+unsat_codes <- seq(1,13)
+unsat_names <- c("Improperly Collected",
+                 "Scratched or Abraded",
+                 "Wet",
+                 "Oversaturated",
+                 "Contaminated",
+                 "No Blood",
+                 "Insufficient Information",
+                 "Old Sample > 10 days in transit",
+                 "Infant > 6 months old",
+                 "Outdated filter paper card",
+                 "Insufficient Quantity",
+                 "Interfering Substances Present",
+                 "Other")
