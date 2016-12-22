be93b5695ca17d15c79a84a386c6b6c5b9950626
Chris Patrick <ellemsee@gmail.com>
2016-12-21 20:44:23 -0500

    Update hospital_plots.R
    Made improvements to plots

--- hospital_plots.R
+++ hospital_plots.R
@@ -1,10 +1,10 @@
 # Add quarters to the dataframe 
-
+ 
 dd_copy <- dd
 dd_copy$QUARTER <- as.yearqtr(dd_copy$BIRTHDATE, format="%Y%m")
-
+ 
 as.yearqtr(Data$date, format = "%Y-%m-%d")
-
+ 
 # Group by submitter and quarter
 hospital_metrics2 <- dd_copy %>%
   group_by(SUBMITTERNAME, QUARTER) %>%
@@ -15,7 +15,7 @@ hospital_metrics2 <- dd_copy %>%
     unsat_count = sum(!is.na(UNSATCODE)),
     unsat_percent = unsat_count/total_samples
   )
-
+ 
 # Group by quarter for state totals - one approach (unweighted mean)
 state_metrics <- dd_copy %>%
   group_by(QUARTER) %>%
@@ -26,7 +26,7 @@ state_metrics <- dd_copy %>%
     unsat_count =  sum(!is.na(UNSATCODE)),
     unsat_percent = unsat_count/total_samples
   )
-
+ 
 # Filter hospital and state dfs
 hospital_filt <- hospital_metrics2[2:5,]
 hospital_filt <- ungroup(hospital_filt)
@@ -35,28 +35,40 @@ hospital_filt$SUBMITTERNAME <- 'Hospital'
 state_filt <- state_metrics[7:10,]
 state_filt$SUBMITTERNAME <- 'State'
 state_filt <- state_filt[,c(6,1:5)]
-
+ 
 # Bind hospital and state dfs together 
 bound <- rbind(hospital_filt, state_filt)
-
+ 
 # Create line graph of transit time over the past 4 quarters for the submitter
 p1 <- ggplot(bound, aes(x=QUARTER, y=avg_transit_time, colour=SUBMITTERNAME, group=rev(SUBMITTERNAME))) +
   scale_x_yearqtr(format="%Y Q%q", n=4) +
   labs(x="Quarter", y="Days") +
+  theme(axis.title.y = element_text(size = rel(1.5))) +
+  theme(axis.title.x = element_text(size = rel(1.5))) +
+  theme(axis.text.x = element_text(size = rel(1.3))) +
+  theme(axis.text.y = element_text(size = rel(1.3))) +
   ggtitle(bquote(atop("Average Transit Time", atop(italic(.(h)), "")))) + 
   geom_line(size=2) +
-  scale_color_manual(values=c("blue","grey75")) +
-  geom_hline(color="green4", aes(yintercept=2)) +
-  geom_text(color="green4", aes(x=2015.6, y=2, label="Goal: under 2 days", vjust=1.25)) +
-  theme(legend.title=element_blank())
+  scale_color_manual(values=c("blue","grey59")) +
+  geom_hline(color="green4", aes(yintercept=2), size=2) +
+  geom_text(color="green4", size=4, aes(x=2015.6, y=2, label="Goal: under 2 days", vjust=1.5)) +
+  theme(legend.title=element_blank()) +
+  theme(legend.text = element_text(size = 15)) +
+  theme(plot.title = element_text(size = 20, face = "bold"))
 p1
-
+ 
 # Create a line graph of unsatisfactory percentage over the past 4 quarters for the submitter
 p2 <- ggplot(bound, aes(x=QUARTER, y=unsat_percent, colour=SUBMITTERNAME, group=rev(SUBMITTERNAME))) +
   scale_x_yearqtr(format="%Y Q%q", n=4) +
   labs(x="Quarter", y="Percentage") +
   ggtitle(bquote(atop("Percentage of Unsatisfactory Samples", atop(italic(.(h)), "")))) +
   geom_line(size=2) +
-  scale_color_manual(values=c("blue","grey75")) +
-  theme(legend.title=element_blank())
+  scale_color_manual(values=c("blue","grey59")) +
+  theme(legend.title=element_blank()) +
+  theme(axis.title.y = element_text(size = rel(1.5))) +
+  theme(axis.title.x = element_text(size = rel(1.5))) +
+  theme(axis.text.x = element_text(size = rel(1.3))) +
+  theme(axis.text.y = element_text(size = rel(1.3))) +
+  theme(legend.text = element_text(size = 15)) +
+  theme(plot.title = element_text(size = 20, face = "bold"))
 p2
