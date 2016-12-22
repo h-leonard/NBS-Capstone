755b2a5cc67d2f529e46c049484e21c6fea7ae4c
Chris Patrick <ellemsee@gmail.com>
2016-12-21 20:26:23 -0500

    Update run_file_and_variable_setting.R
    Changed path for code csv files to include both submitter and unsat codes

--- run_file_and_variable_setting.R
+++ run_file_and_variable_setting.R
@@ -1,70 +1,72 @@
 ##### Change the settings below as needed #####
-
+ 
 # SET WORKING DIRECTORY
 # This should be where you have all of your R code files stored
-
+ 
 wd <- "r_files"
-
+ 
 #########
-
+ 
 # DATA FILE PATH
 # This should be your directory (e.g., the location on your 
 # computer) where you have the data stored
-
+ 
 data_path <- "data"
-
+ 
 #########
-
+ 
 # SEPARATOR
 # Enter the separator (e.g., '|' or ',' that is used to separate between fields in your data files)
-
+ 
 separator <- "|"
-
+ 
 #########
-
-# SUBMITTER NAMES FILE PATH
+ 
+# CODES FILE PATH
 # This should be your directory (e.g., the location on your
-# computer) where you have the file with the submitter names (as you wish them
-# to appear) and the IDs for the submitters
-
-submitters_path <- "submitters"
-
+# computer) where you have all supporting csv files. Required
+# at this location are two files:
+# -- VA NBS Report Card Hospital Names.csv - has hospital codes & names
+# -- unsat_codes.csv - has descriptions for each unsatisfactory code
+ 
+codes_path <- "submitter_and_unsat_codes"
+ 
 #########
-
+ 
 # REPORT OUTPUT DIRECTORY
 # Enter the location on your computer where you want the hospital
 # reports to be saved
-
+ 
 output_path <- "/mnt/reports"
-
+ 
 ########
-
+ 
 # DATES FOR REPORT
 # Enter the dates you want to use for generating the hospital reports
 # Use "mm/dd/yyyy" format (e.g., "04/01/2016")
-
+ 
 start_date <- "07/01/2016"
 end_date <- "09/30/2016"
-
+ 
 ########
-
+ 
 # QUARTERLY OR MONTHLY
 # Enter "quarterly" or "monthly" to define the parameters 
 # for the line chart for transit time and unsat count 
-
+ 
 line_chart <- "quarterly"
-
+ 
 #########
-
+ 
 # FUNCTIONALITY TEST
 # Do you want to run a report for a single submitter to test functionality,
 # or do you want reports for all submitters? Enter "Y" if you wish to run
 # only a single report, "N" otherwise
-
+ 
 test_report <- "N"
-
+ 
 #########
-
+ 
 # RUN REPORT - do not change this code
 load_packages <- paste(wd, "/load_packages.R", sep="")
 run_file <- paste(wd, "/r_script_pdf.R", sep="")
