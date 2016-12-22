6b2c22dd1967c27ad0142834082a4ec2658940c0
Chris Patrick <ellemsee@gmail.com>
2016-12-21 20:29:03 -0500

    Update r_script_pdf.R
    Read unsat codes csv

--- r_script_pdf.R
+++ r_script_pdf.R
@@ -2,33 +2,36 @@
 # FILE LOCATION AND NAMES ARE SET IN "run_file_and_variable_setting.R."
 # IF YOU RUN THAT FILE, IT WILL ALLOW YOU TO SET YOUR FILE LOCATIONS
 # AND ALSO RUN THIS FILE.
-
+ 
+# read in unsats csv file
+unsats <- read.csv(paste(codes_path, "/", "unsat_codes.csv", sep=""))
+ 
+# read in submitter names as we wish them to appear in the report
+temp2 <- paste(codes_path, "/", "VA NBS Report Card Hospital Names.csv", sep="")
+submitters <- as.data.frame(read.csv(temp2, sep=","))
+names(submitters) <- c("HOSPITALCODE","HOSPITALREPORT")
+submitters$HOSPITALCODE <- as.character(submitters$HOSPITALCODE)
+ 
 # read in data
 files <- list.files(data_path, pattern="*.txt")
 temp <- paste(data_path, "/", files, sep="")
 dd <- do.call(rbind, lapply(temp, function(x) read.csv(x, stringsAsFactors = FALSE, sep=separator)))
-
+ 
 # reformat dates as dates
 dd$COLLECTIONDATE <- as.Date(dd$COLLECTIONDATE, "%m/%d/%Y", origin = "1904-01-01")
 dd$BIRTHDATE <- as.Date(dd$BIRTHDATE, "%m/%d/%Y", origin = "1904-01-01")
 start_date <- as.Date(start_date, "%m/%d/%Y")
 end_date <- as.Date(end_date, "%m/%d/%Y")
-
-# read in submitter names as we wish them to appear in the report
-temp2 <- paste(submitters_path, "/", "VA NBS Report Card Hospital Names.csv", sep="")
-submitters <- as.data.frame(read.csv(temp2, sep=","))
-names(submitters) <- c("HOSPITALCODE","HOSPITALREPORT")
-submitters$HOSPITALCODE <- as.character(submitters$HOSPITALCODE)
-
+ 
 # add hospitalreport name to dd
 dd <- left_join(dd, submitters, by="HOSPITALCODE")
-
+ 
 # MAY NEED TO ADD ADDITIONAL CODE TO PULL IN 
 # SUBMITTER NAME BY SUBMITTER ID IF HOSPITAL CODE IS MISSING
-
+ 
 # replace submitter name with hospitalreport field
 dd$SUBMITTERNAME <- dd$HOSPITALREPORT
-
+ 
 # create data frame of required metrics for each submitter
 hospital_metrics <- dd[!is.na(dd$HOSPITALREPORT),] %>%
   group_by(SUBMITTERNAME) %>%
@@ -65,11 +68,11 @@ hospital_metrics <- dd[!is.na(dd$HOSPITALREPORT),] %>%
     unsat_12 = ifelse(sum(UNSATCODE == 12, na.rm=TRUE) != 0, sum(UNSATCODE == 12, na.rm=TRUE), NA),
     unsat_13 = ifelse(sum(UNSATCODE == 13, na.rm=TRUE) != 0, sum(UNSATCODE == 13, na.rm=TRUE), NA)
   )
-
-
+ 
+ 
 # Overall unsat percentage:
 unsatp <- (sum(!is.na(dd$UNSATCODE)))/nrow(dd)
-
+ 
 # Calculate percent of specific unsatisfactory samples
 unsat1 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 1])))/nrow(dd)
 unsat2 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 2])))/nrow(dd)
@@ -84,44 +87,44 @@ unsat10 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 10])))/nrow(dd)
 unsat11 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 11])))/nrow(dd)
 unsat12 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 12])))/nrow(dd)
 unsat13 <- (sum(!is.na(dd$UNSATCODE[dd$UNSATCODE == 13])))/nrow(dd)
-
-
+ 
+ 
 # Unsat samples per each hospital for previous 3 quarters
-
-#Calculate quarter end from user inputted start date
+ 
+ #Calculate quarter end from user inputted start date
 quarter_end1 <- as.Date(as.yearqtr(start_date,"%m/%d/%Y") - 1/4, frac = 1)
 quarter_end2 <- as.Date(as.yearqtr(start_date, "%m/%d/%Y") - 1/2, frac = 1)
 quarter_end3 <- as.Date(as.yearqtr(start_date, "%m/%d/%Y") - 3/4, frac = 1)
-
+ 
 # Calculate quarter start date from each quarter end date
 quarter_start1 <- as.Date(as.yearqtr(quarter_end1) - 1/4, frac = 1) + 1
 quarter_start2 <- as.Date(as.yearqtr(quarter_end2) - 1/4, frac = 1) + 1
 quarter_start3 <- as.Date(as.yearqtr(quarter_end3) - 1/4, frac = 1) + 1
-
-
-
+ 
+ 
+ 
 # Rank hospitals by mean transit time (ascending order; e.g., least mean transit time = #1)
 hospital_metrics$rank_transit <- rank(hospital_metrics$avg_transit_time[hospital_metrics$SUBMITTERNAME != "NA"], 
                                       na.last="keep", ties.method="min")
-
+ 
 # Rank hospitals by percentage of samples recevied within 2 days (descending order; e.g.,
 #      greatest percentage of samples received by target time = #1)
 hospital_metrics$rank_percent_within_goal <- rank(-hospital_metrics$rec_in_2_days[hospital_metrics$SUBMITTERNAME != "NA"], 
                                                   na.last="keep", ties.method="min")
-
+ 
 # Rank hospitals by number of samples collected at less than 24 hours of age (ascending order;
 #     e.g., least number of early collections = #1)
 hospital_metrics$rank_early_collection <- rank(hospital_metrics$col_less_than_24_hours[hospital_metrics$SUBMITTERNAME != "NA"], 
                                                na.last="keep", ties.method="min")
-
+ 
 # Rank hospitals by number of unsatisfactory samples (ascending order; e.g., least number of unsats = #1)
 hospital_metrics$rank_unsats <- rank(hospital_metrics$unsat_count[hospital_metrics$SUBMITTERNAME != "NA"], 
                                      na.last="keep", ties.method="min")
-
+ 
 # Change hospital metrics to include only a single submitter (if we are only testing the functionality rather
 # than running all reports)
 if (test_report == "Y") hospital_metrics = hospital_metrics[1,]
-
+ 
 # Create metrics for state
 state <- dd %>%
   select(TRANSIT_TIME, COLLECTIONDATE, COLLECTIONTIME, BIRTHDATE, BIRTHTIME, 
@@ -143,20 +146,20 @@ state <- dd %>%
     unsat_count = sum(!is.na(UNSATCODE)),
     unsat_percent = unsat_count/total_samples
   )
-
+ 
 # Add number of hospitals to state df
 state$submitters <- nrow(hospital_metrics)
-
+ 
 # Generate report for each hospital
 render_file <- paste(wd, "/", "r_script_pdf.Rmd", sep="")
-
+ 
 for (submitter in hospital_metrics$SUBMITTERNAME){
   rmarkdown::render(input = render_file, 
                     output_format = "pdf_document",
-                    output_file = paste("test_report_", submitter, "_", Sys.Date(), ".pdf", sep=''),
+                    output_file = paste(submitter, "_", Sys.Date(), ".pdf", sep=''),
                     output_dir = output_path)
 }
-
+ 
 # Remove variables from environment
 # keep = 'dd'
 # keep = ''
