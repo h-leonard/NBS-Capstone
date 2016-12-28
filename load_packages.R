3b6be1b0868ed6c92173e527844cfe9c0fadfb7b
Chris Patrick <ellemsee@gmail.com>
2016-12-28 15:38:48 -0500

    Update load_packages.R
    Added 'reshape2' library for transforming unsat count dataframe, 'stringr' for pasting leading 0s for use in column names, and data.table for cross join function

--- load_packages.R
+++ load_packages.R
@@ -1,7 +1,10 @@
 # Load packages for Newborn Screening Hospital Reporting
 # Do not access this file directly; "run_file_and_variable_setting.R"
 # will automatically run this file.
-
+ 
+suppressPackageStartupMessages(require('data.table'))
+suppressPackageStartupMessages(require('stringr'))
+suppressPackageStartupMessages(require('reshape2'))
 suppressPackageStartupMessages(require('knitr'))
 suppressPackageStartupMessages(require('markdown'))
 suppressPackageStartupMessages(require('zoo'))
