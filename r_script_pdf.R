5d06fc541ae3d904adcb5bd58938bcf44fd48956
Chris Patrick <ellemsee@gmail.com>
2017-01-05 13:25:11 -0500

    Update r_script_pdf.R
    Added ability to read in an xlsx file as data source

--- r_script_pdf.R
+++ r_script_pdf.R
@@ -2,42 +2,47 @@
 # FILE LOCATION AND NAMES ARE SET IN "run_file_and_variable_setting.R."
 # IF YOU RUN THAT FILE, IT WILL ALLOW YOU TO SET YOUR FILE LOCATIONS
 # AND ALSO RUN THIS FILE.
- 
+
 # read in unsats csv file
 unsats <- read.csv(paste(codes_path, "/", "unsat_codes.csv", sep=""))
- 
+
 # read in submitter names as we wish them to appear in the report
 temp <- paste(codes_path, "/", "VA NBS Report Card Hospital Names.csv", sep="")
 submitters <- as.data.frame(read.csv(temp, sep=","))
 names(submitters) <- c("SUBMITTERID","HOSPITALREPORT")
 submitters$SUBMITTERID <- as.character(submitters$SUBMITTERID)
- 
+
 # read in data
-files <- list.files(data_path, pattern="*.txt")
+pattern <- paste("*.", data_type, sep="")
+files <- list.files(data_path, pattern=pattern)
 temp2 <- paste(data_path, "/", files, sep="")
-initial_dd <- do.call(rbind, lapply(temp2, function(x) read.csv(x, stringsAsFactors = FALSE, sep=separator)))
- 
+if (data_type == "xlsx") {
+  initial_dd <- do.call(rbind, lapply(temp2, function(x) readWorksheet(loadWorkbook(x), sheet = 1, header=TRUE)))
+} else {
+  initial_dd <- do.call(rbind, lapply(temp2, function(x) read.csv(x, stringsAsFactors = FALSE, sep=separator)))
+}
+
 # remove any records that have category listed as "Proficiency", "Treatment", or "Treatment - PKU"
 remove_cats <- c("Proficiency","Treatment","Treatment - PKU")
 dd <- filter(initial_dd, !(CATEGORY %in% remove_cats))
- 
+
 # reformat dates as dates
 dd$COLLECTIONDATE <- as.Date(dd$COLLECTIONDATE, "%m/%d/%Y", origin = "1904-01-01")
 dd$BIRTHDATE <- as.Date(dd$BIRTHDATE, "%m/%d/%Y", origin = "1904-01-01")
 start_date <- as.Date(start_date, "%m/%d/%Y")
 end_date <- as.Date(end_date, "%m/%d/%Y")
- 
+
 # add hospitalreport name to dd
 dd <- left_join(dd, submitters, by="SUBMITTERID")
- 
+
 # replace submitter name with hospitalreport field
 dd$SUBMITTERNAME <- dd$HOSPITALREPORT
- 
+
 # remove records that have NA for submittername and that are not in the target period
 dd <- dd[!is.na(dd$HOSPITALREPORT),]
- 
+
 ##############################
- 
+
 # create data frame of required metrics for each submitter
 hospital_metrics <- dd %>%
   group_by(SUBMITTERNAME) %>%
@@ -62,62 +67,62 @@ hospital_metrics <- dd %>%
     unsat_count = sum(!is.na(UNSATCODE)),
     unsat_percent = unsat_count/total_samples
   )
- 
+
 ##### ADD UNSAT COUNTS #####
- 
+
 # get count of unsat codes for each submitter
 unsat_prep <- dd[!is.na(dd$UNSATCODE),] %>% 
   group_by(SUBMITTERNAME, UNSATCODE) %>%
   filter(BIRTHDATE >= start_date & BIRTHDATE <= end_date) %>%
   summarise(count = n())  
- 
+
 # get all possibilities for unsat codes
 unsat_seq <- seq(1:nrow(unsats))
- 
+
 # create cross join of all possible unsat codes and all submitter names
 cross_join <- CJ(SUBMITTERNAME=unique(dd$SUBMITTERNAME), UNSATCODE=unsat_seq)
- 
+
 # create left join of unsat counts and cross_join (so we have 0 counts
 # for each submitter that has no unsats for that particular code)
 unsat_amts <- left_join(cross_join, unsat_prep, by=c("SUBMITTERNAME", "UNSATCODE"))
- 
+
 # replace UNSATCODE column with 'col' column
 unsat_amts$col <- paste("unsat_", str_pad(unsat_amts$UNSATCODE, 2, pad="0"), sep="")
 unsat_amts$UNSATCODE <- NULL
- 
+
 # Reshape dataframe to have rows as SUBMITTERNAME and columns as col (e.g., unsat_01, unsat_02, etc.)
 unsats_ready <- dcast(unsat_amts, SUBMITTERNAME ~ col, value.var="count")
- 
+
 # left join unsats_ready and hospital_metrics
 hospital_metrics <- left_join(hospital_metrics, unsats_ready, by="SUBMITTERNAME")
- 
+
 ############################
- 
+
 # Rank hospitals by mean transit time (ascending order; e.g., least mean transit time = #1)
 hospital_metrics$rank_transit <- rank(hospital_metrics$avg_transit_time, na.last="keep", ties.method="min")
- 
+
 # Rank hospitals by percentage of samples recevied within 2 days (descending order; e.g.,
 #      greatest percentage of samples received by target time = #1)
 hospital_metrics$rank_percent_within_goal <- rank(-hospital_metrics$percent_rec_in_2_days, na.last="keep", ties.method="min")
- 
+
 # Rank hospitals by number of samples collected at less than 24 hours of age (ascending order;
 #     e.g., least number of early collections = #1)
 hospital_metrics$rank_early_collection <- rank(hospital_metrics$percent_less_than_24_hours, na.last="keep", ties.method="min")
- 
+
 # Rank hospitals by number of samples transfused prior to collection (ascending order;
 #     e.g., least number of early collections = #1)
 hospital_metrics$rank_transfused <- rank(hospital_metrics$trans_percent, na.last="keep", ties.method="min")
- 
+
 # Rank hospitals by number of unsatisfactory samples (ascending order; e.g., least number of unsats = #1)
 hospital_metrics$rank_unsats <- rank(hospital_metrics$unsat_percent, na.last="keep", ties.method="min")
- 
+
 # Determine number of hospitals
 tot_sub <- nrow(hospital_metrics)
- 
+
 # Change hospital metrics to include only a single submitter (if we are only testing the functionality rather
 # than running all reports)
 if (test_report == "Y") hospital_metrics = hospital_metrics[1,]
- 
+
 # Create metrics for state
 state <- dd %>%
   select(TRANSIT_TIME, COLLECTIONDATE, COLLECTIONTIME, BIRTHDATE, BIRTHTIME, 
@@ -140,10 +145,10 @@ state <- dd %>%
     unsat_count = sum(!is.na(UNSATCODE)),
     unsat_percent = unsat_count/total_samples
   )
- 
+
 # Add number of hospitals to state df
 state$submitters <- tot_sub
- 
+
 ##### CREATE DATA FRAMES FOR USE IN QUARTER PLOTS #####
 if (line_chart == "quarterly") {
   quarter_end <- as.Date(as.yearqtr(end_date), frac=1)
@@ -154,7 +159,7 @@ if (line_chart == "quarterly") {
   
   # add quarter information to dd_plot
   dd_plot$QUARTER <- as.yearqtr(dd_plot$BIRTHDATE, format="%Y%m")
- 
+
   # group by submitter and quarter
   hospital_metrics_plot <- dd_plot %>%
     group_by(SUBMITTERNAME, QUARTER) %>%
@@ -179,7 +184,7 @@ if (line_chart == "quarterly") {
   hospital_metrics_plot <- full_join(hospital_metrics_plot, cross_join_quarts, by=c("SUBMITTERNAME", "QUARTER"))
   
   #####
- 
+
   # determine limits for y-axes by finding max for avg_transit_time
   # and percentage of unsats and min for avg_transit_time. For avg_transit_time use
   # only the greatest value that is less than 4; for unsat percentage,
@@ -262,19 +267,19 @@ if (line_chart == "monthly") {
     )
   state_plot$SUBMITTERNAME <- 'State'
 }
- 
+
 #######################################################
- 
+
 # Generate report for each hospital
 render_file <- paste(wd, "/", "r_script_pdf.Rmd", sep="")
- 
+
 for (submitter in hospital_metrics$SUBMITTERNAME){
   rmarkdown::render(input = render_file, 
                     output_format = "pdf_document",
                     output_file = paste(submitter, "_", Sys.Date(), ".pdf", sep=''),
                     output_dir = output_path)
 }
- 
+
 # Remove variables from environment
 # keep = 'dd'
 # keep = ''
