4fb8ac4a0afc7f0173722afed3ba4f4ce53a0832
Chris Patrick <ellemsee@gmail.com>
2017-01-14 13:12:55 -0500

    Update load_packages.R
    Added pander for table processing

--- load_packages.R
+++ load_packages.R
@@ -1,23 +1,24 @@
 # Load packages for Newborn Screening Hospital Reporting
 # Do not access this file directly; "run_file_and_variable_setting.R"
 # will automatically run this file.
-
+ 
 libs <- c('XLConnect', 
-              'xtable',
-              'data.table',
-              'stringr',
-              'reshape2',
-              'knitr',
-              'markdown',
-              'zoo',
-              'ggplot2',
-              'grid',
-              'reshape',
-              'lubridate',
-              'dplyr',
-              'shiny',
-              'toOrdinal')
-
+          'xtable',
+          'data.table',
+          'stringr',
+          'reshape2',
+          'knitr',
+          'markdown',
+          'zoo',
+          'ggplot2',
+          'grid',
+          'reshape',
+          'lubridate',
+          'dplyr',
+          'pander',
+          'shiny',
+          'toOrdinal')
+ 
 for (l in libs) {
   if(!is.element(l, .packages(all.available = TRUE)) ) {
     install.packages(l)
