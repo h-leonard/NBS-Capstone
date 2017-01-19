b6adcddb2448a9663b53f4e6025b3578b44cc1ce
Chris Patrick <ellemsee@gmail.com>
2017-01-19 11:54:39 -0500

    Update diagnosis_report_generator.R
    Added function for reading diagnosis data

--- diagnosis_report_generator.R
+++ diagnosis_report_generator.R
@@ -6,22 +6,8 @@ diag_desc <- diag_desc[,2:3]
 messages <- read.csv(paste(codes_path, "/", "hospital_messages.csv", sep=""), stringsAsFactors = FALSE)
 messages <- messages[messages$Message != "",]
  
-# prepare to read in data
-files_diag <- list.files(diag_data_path)
-temp_diag <- paste(diag_data_path, "/", files_diag, sep="")
- 
-# find out what type of data files we have by getting the file (assumes all data files are same file type)
-data_type_diag <- substr(files_diag[1], as.numeric(regexpr("\\.([[:alnum:]]+)$", files_diag[1])[1]), nchar(files_diag[1]))
- 
-# read in data using different methods depending on what type of data files we have (e.g., .xls vs. .txt)
-if (data_type_diag == ".xlsx" | data_type_diag == ".xls") {
-  initial_dd_diag <- do.call(rbind, lapply(temp_diag, function(x) readWorksheet(loadWorkbook(x), sheet = 1, header=TRUE)))
-} else {
-  initial_dd_diag <- do.call(rbind, lapply(temp_diag, function(x) read.csv(x, stringsAsFactors = FALSE, sep=separator)))
-}
- 
-# reformat dates as dates
-initial_dd_diag$DIAGNOSISDATE <- as.Date(initial_dd_diag$DIAGNOSISDATE, "%m/%d/%Y", origin = "1904-01-01")
+# read in sample data and reformat DIAGNOSISDATE as dates
+initial_dd_diag <- read_data(diag_data_path, "DIAGNOSISDATE")
  
 # filter diagnoses based on start date and end date
 dd_diag <- initial_dd_diag %>%
