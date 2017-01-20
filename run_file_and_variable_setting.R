b03f80f9ae6cf716b7e209d2e0297f23dc3bfdcc
Chris Patrick <ellemsee@gmail.com>
2017-01-20 14:22:35 -0500

    Update run_file_and_variable_setting.R
    Changed variable values to match new project environment

--- run_file_and_variable_setting.R
+++ run_file_and_variable_setting.R
@@ -1,100 +1,100 @@
 ##### Change the settings below as needed #####
- 
+
 # SET WORKING DIRECTORY
 # This should be where you have all of your R code files stored
- 
-wd <- "report_card_r_files"
- 
+
+wd <- "/mnt/cpp4f/NBS_Report_Card/report_card_r_files"
+
 #########
- 
+
 # SAMPLE DATA FILE PATH
 # This should be your directory (e.g., the location on your 
 # computer) where you have the SAMPLE data stored. 
 # **VERY IMPORTANT**: make sure that you ONLY have the sample data
 # files you want to read in at this location, and that all of them are in the 
 # same exact format (e.g., xlsx, xls, txt, or csv).
- 
-sample_data_path <- "data/Report_data"
- 
+
+sample_data_path <- "/mnt/cpp4f/Newborn_Screening_Data/Report_data"
+
 #########
- 
+
 # DIAGNOSIS DATA FILE PATH
 # This should be your directory (e.g., the location on your 
 # computer) where you have the DIAGNOSIS data stored. 
 # **VERY IMPORTANT**: make sure that you ONLY have the diagnosis data
 # files you want to read in at this location, and that all of them are in the 
 # same exact format (e.g., xlsx, xls, txt, or csv).
- 
-diag_data_path <- "data/Diagnosis_data"
- 
+
+diag_data_path <- "/mnt/cpp4f/Newborn_Screening_Data/Diagnosis_data"
+
 #########
- 
+
 # SEPARATOR
 # Enter the separator (e.g., '|' or ',' that is used to separate between fields in your data files)
- 
+
 separator <- "|"
- 
+
 #########
- 
+
 # CODES FILE PATH
 # This should be your directory (e.g., the location on your
 # computer) where you have all supporting csv files. Required
 # at this location are two files:
 # -- VA NBS Report Card Hospital Names.csv - has hospital codes & names
 # -- unsat_codes.csv - has descriptions for each unsatisfactory code
- 
-codes_path <- "submitter_and_unsat_codes"
- 
+
+codes_path <- "/mnt/cpp4f/NBS_Report_Card/submitter_and_unsat_codes"
+
 #########
- 
+
 # REPORT OUTPUT DIRECTORY (FOR HOSPITALS)
 # Enter the location on your computer where you want the hospital
 # reports to be saved
- 
-hospital_path <- "/mnt/hospital_reports"
- 
+
+hospital_path <- "/mnt/cpp4f/NBS_Report_Card/hospital_reports"
+
 ########
- 
+
 # ADMINISTRATIVE REPORT OUTPUT DIRECTORY (FOR DCLS)
 # Enter the location on your computer where you want administrative
 # reports to be saved
- 
-admin_path <- "/mnt/admin_reports"
- 
+
+admin_path <- "/mnt/cpp4f/NBS_Report_Card/admin_reports"
+
 ########
- 
+
 # DATES FOR REPORT
 # Enter the dates you want to use for generating the hospital reports
 # Use "mm/dd/yyyy" format (e.g., "04/01/2016")
- 
+
 start_date <- "07/01/2016"
 end_date <- "09/30/2016"
- 
+
 ########
- 
+
 # QUARTERLY OR MONTHLY
 # Enter "quarterly" or "monthly" to define the parameters 
 # for the line chart for transit time and unsat count 
- 
-line_chart <- "quarterly"
- 
+
+line_chart <- "monthly"
+
 #########
- 
+
 # FUNCTIONALITY TEST
 # Do you want to run a report for a single submitter to test functionality,
 # or do you want reports for all submitters? Enter "Y" if you wish to run
 # only a single report, "N" otherwise
- 
+
 test_report <- "N"
- 
+
 #########
- 
+
 # *** DO NOT CHANGE THIS CODE ***
- 
+
 # LOAD PACKAGES AND FUNCTIONS
 load_packages <- paste0(wd, "/load_packages_and_functions.R")
 source(load_packages)
- 
+
 # GENERATE LIST OF DIAGNOSES IN DATA SET THAT NEED NARRATIVES
 # BEFORE RUNNING DIAGNOSIS REPORTS - THIS WILL OUTPUT A CSV
 # TO THE FOLDER YOU DESIGNATE IN THE admin_path VARIABLE
@@ -102,7 +102,7 @@ source(load_packages)
 # this code)
 diag_narr_test <- paste0(wd, "/diagnosis_narrative_test.R")
 source(diag_narr_test)
- 
+
 # RUN REPORTS
 report_run <- paste0(wd, "/main_report_generator.R")
 diag_run <- paste0(wd, "/diagnosis_report_generator.R")
