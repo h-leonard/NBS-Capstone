ec4630fb17464c7abaadb0564cef014f9bafa655
Chris Patrick <ellemsee@gmail.com>
2017-01-21 15:53:53 -0500

    Update load_packages_and_functions.R
    Added readxl package and removed XLConnect for the purpose of reading XLS and XLSX files. Also revised read_data function to work with either XLS/XLSX (readxl) or csv/txt (read.csv).

--- load_packages_and_functions.R
+++ load_packages_and_functions.R
@@ -1,9 +1,8 @@
 # Load packages and functions for Newborn Screening Hospital Reporting
 # Do not access this file directly; "run_file_and_variable_setting.R"
 # will automatically run this file.
- 
-libs <- c('XLConnect', 
-          'xtable',
+
+libs <- c('xtable',
           'data.table',
           'stringr',
           'reshape2',
@@ -18,15 +17,16 @@ libs <- c('XLConnect',
           'pander',
           'shiny',
           'toOrdinal',
-          'mailR')
- 
+          'mailR',
+          'readxl')
+
 for (l in libs) {
   if(!is.element(l, .packages(all.available = TRUE)) ) {
     install.packages(l)
   }
   suppressPackageStartupMessages(library(l, character.only=TRUE))
 }
- 
+
 get_file_list <- function(folder) {
   
   # Returns list of files in a folder
@@ -38,7 +38,7 @@ get_file_list <- function(folder) {
   return(temp)
   
 }
- 
+
 get_file_extension <- function(folder) {
   
   # Returns the file extension of files in a folder. Assumes that all files have
@@ -54,10 +54,12 @@ get_file_extension <- function(folder) {
   return(data_type)
   
 }
- 
+
 read_data <- function(folder, ...) {
   
-  # Returns dataframe of data. Optional arguments are columns to be reformatted as dates.
+  # Returns dataframe of data. Optional arguments are columns to be reformatted as dates
+  # (for use with csv and txt files). When reading Excel files, all columns featuring
+  # POSIXct data are automatically reformatted as dates.
   
   # Make list of columns to be reformatted as dates
   date_reformat = list(...)
@@ -70,22 +72,34 @@ read_data <- function(folder, ...) {
   
   # read in data using different methods depending on what type of data files we have (e.g., .xls vs. .txt)
   if (ext == ".xlsx" | ext == ".xls") {
-    initial_dd <- do.call(rbind, lapply(temp, function(x) readWorksheet(loadWorkbook(x), sheet = 1, header=TRUE)))
+    initial_dd <- do.call(rbind, lapply(temp, function(x) read_excel(x, sheet = 1)))
   } else {
     initial_dd <- do.call(rbind, lapply(temp, function(x) read.csv(x, stringsAsFactors = FALSE, header=TRUE, sep=separator)))
   }
   
-  # reformat dates as dates
+  # Reformat any POSIXct columns as dates
+  initial_dd[] <- lapply(initial_dd, function(x) {
+    if (inherits(x, "POSIXct")) as.Date(x) else x
+    })
+  
+  # Reformat any specified columns as dates
   if (!is.null(date_reformat)) {
     for (i in date_reformat) {
-      initial_dd[,i] <- as.Date(initial_dd[,i], "%m/%d/%Y", origin = "1904-01-01")
+      if (class(initial_dd[,eval(i)][[1]]) != "Date") {
+        initial_dd[,i] <- as.Date(initial_dd[,i], "%m/%d/%Y", origin = "1904-01-01")
+      }
     }
+  }
+  
+  # Reformat SUBMITTERID as character
+  if("SUBMITTERID" %in% names(initial_dd)) {
+    initial_dd$SUBMITTERID <- as.character(initial_dd$SUBMITTERID)
   }
   
   return(initial_dd)
   
 }
- 
+
 # Reformat start date and end date as dates
 start_date <- as.Date(start_date, "%m/%d/%Y")
 end_date <- as.Date(end_date, "%m/%d/%Y")
