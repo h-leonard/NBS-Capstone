060cd7c555089a36764ffd878c6bd4d756d4eb6a
Chris Patrick <ellemsee@gmail.com>
2017-02-04 17:06:03 -0500

    Update send_emails.R
    Redone to allow for sending emails via Outlook. Requires Outlook to be open and user must give permission for application to send.

--- send_emails.R
+++ send_emails.R
@@ -1,38 +1,24 @@
-library(mailR)
-
-# load email addresses for hospitals
-emails <- read.csv("/Users/chrispatrick/Documents/Classes/Fall 2016/DS 6001/Newborn Screening/Email/hospital_emails.csv", 
-                   stringsAsFactors=FALSE, header=TRUE)
-
+# load in email addresses for hospitals
+emails <- read.csv(paste(codes_path, "/", "hospital_emails.csv", sep=""), stringsAsFactors=FALSE,
+                   header=TRUE)
+ 
 # remove rows with no email addresses
 emails <- emails[emails$Email != "",]
-
-sender <- ** ADD EMAIL HERE **
-password <- ** ADD PASSWORD HERE **
+ 
+# set email parameters
 subject <- "Quarterly Report from DCLS: 7/1/2016 - 9/30/2016"
 message <- "Hi,\n\nPlease find attached the new hospital report card from the Division of Consolidated Laboratory Services. If your hospital had any diagnoses for the given period, you will also find a diagnosis report attached.\n\nSincerely,\nDCLS"
-
+ 
 testing <- "N"
-test_email_recipient <- ** ADD EMAIL HERE **
-
+test_email_recipient <- "ellemsee@gmail.com"
+ 
 email_end <- ifelse(testing == 'Y', 1, nrow(emails))
-
+ 
 for (i in 1:email_end) {
-  attachments <- paste("/Users/chrispatrick/Desktop/", 
-                       list.files("/Users/chrispatrick/Desktop/", 
+  attachments <- paste(hospital_path, "/",
+                       list.files(hospital_path, 
                                   pattern=paste0(emails$Hospital[i], "*")), sep="")
-  if(!is.list(attachments) && attachments == "/Users/chrispatrick/Desktop/") next
-  recipients <- ifelse(testing == 'Y', test_email_recipient, unlist(strsplit(emails$Email[i], ",")))
-  send.mail(from=sender, 
-            to=recipients,
-            subject=subject,
-            body=message,
-            smtp=list(host.name="smtp.gmail.com", 
-                      port=465,                            
-                      user.name=sender,
-                      passwd=password,
-                      ssl=TRUE),
-            authenticate=TRUE,
-            send=TRUE,
-            attach.files=attachments)
+  if(!is.list(attachments) && attachments == hospital_path) next
+  recipients <- ifelse(testing == 'Y', test_email_recipient, emails$Email[i])
+  sendEmail(recipients, subject, message, attachments)
 }
