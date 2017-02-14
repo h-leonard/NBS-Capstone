cb52d7b7b55ea94bdef1841595315dcab7b7ed9c
Chris Patrick <ellemsee@gmail.com>
2017-02-14 18:41:39 -0500

    Update load_packages_and_functions.R
    Change loading for RDCOMClient so will automatically load if computer is PC, don't have to uncomment the code to allow it to run

--- load_packages_and_functions.R
+++ load_packages_and_functions.R
@@ -1,7 +1,7 @@
 # Load packages and functions for Newborn Screening Hospital Reporting
 # Do not access this file directly; "run_file_and_variable_setting.R"
 # will automatically run this file.
-
+ 
 libs <- c('xtable',
           'data.table',
           'stringr',
@@ -19,22 +19,31 @@ libs <- c('xtable',
           'lazyeval',
           'toOrdinal',
           'readxl',
-          # 'RDCOMClient' Uncomment this code when running on PC
           'rmarkdown')
-
+ 
 for (l in libs) {
   if(!is.element(l, .packages(all.available = TRUE)) ) {
     install.packages(l)
   }
   suppressPackageStartupMessages(library(l, character.only=TRUE))
 }
-
+ 
+# Install RDCOMClient if running on a PC (will not work on 
+# Mac as built for older version of R)
+if (exists("comp_type") && comp_type == 'PC') {
+  if(!is.element('RDCOMClient', .packages(all.available = TRUE)) ) {
+    install.packages('RDCOMClient')
+  }
+  suppressPackageStartupMessages(library('RDCOMClient', character.only=TRUE))
+  
+}
+ 
 # Reformat start date and end date as dates
 if (exists("start_date")) {
   start_date <- as.Date(start_date, "%m/%d/%Y")
   end_date <- as.Date(end_date, "%m/%d/%Y")
 }
-
+ 
 get_file_list <- function(folder) {
   
   # Returns list of files in a folder
@@ -46,7 +55,7 @@ get_file_list <- function(folder) {
   return(temp)
   
 }
-
+ 
 get_file_extension <- function(folder) {
   
   # Returns the file extension of files in a folder. Assumes that all files have
@@ -62,7 +71,7 @@ get_file_extension <- function(folder) {
   return(data_type)
   
 }
-
+ 
 read_data <- function(folder, ...) {
   
   # Returns dataframe of data. Optional arguments are columns to be reformatted as dates
@@ -123,7 +132,7 @@ read_data <- function(folder, ...) {
   return(initial_dd)
   
 }
-
+ 
 create_filt_dfs <- function(df, type=c("sample","diagnosis"), s_date=start_date, e_date=end_date, period=line_chart) {
   
   # Given a dataframe, start date, and end date, returns 2 data frames filtered by
@@ -164,7 +173,7 @@ create_filt_dfs <- function(df, type=c("sample","diagnosis"), s_date=start_date,
   return(list(period_df, year_df))
   
 }
-
+ 
 sendEmail <- function(to, subject, msgBody, file=NULL) {
   
   # Sends emails through Outlook client. Outlook needs to be open and will
@@ -190,7 +199,7 @@ sendEmail <- function(to, subject, msgBody, file=NULL) {
   OutMail$Send()
   
 }
-
+ 
 stopQuietly <- function(...) {
   
   # Stops a source file quietly (without printing an error message), used in cases
