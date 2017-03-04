534e17ac017abbccaefe65de02676278f8d6a384
Chris Patrick <ellemsee@gmail.com>
2017-03-04 13:26:23 -0500

    Update diagnosis_narrative_test.R
    Now sources load_packages and diagnosis_prep. Also updated to test for 'Narrative' rather than 'description.'

--- diagnosis_narrative_test.R
+++ diagnosis_narrative_test.R
@@ -1,24 +1,17 @@
-# read in narratives for diagnoses
-diag_narr <- read.csv(paste(codes_path, slash, "diagnosis_descriptions.csv", sep=""), stringsAsFactors = FALSE)
-diag_narr <- diag_narr[,2:3]
+# Source load_packages file
+load_packages <- paste0(wd, slash, "load_packages_and_functions.R")
+source(load_packages)
  
-# read in diagnosis data
-initial_dd_diag <- read_data(diag_data_path, "DIAGNOSISDATE")
- 
-# join diagnoses and narratives
-dd_diag_narr <- full_join(initial_dd_diag, diag_narr, by="DIAGNOSIS")
- 
-# filter diagnoses based on start date and end date
-dd_diag <- dd_diag_narr %>%
-  filter(DIAGNOSISDATE >= start_date & DIAGNOSISDATE <= end_date)
+# Source diagnosis_prep.R for initial preparation of dataset
+source(paste0(wd, slash, "diagnosis_prep.R"))
   
 # filter diagnoses based on start date and end date
 need_narratives_list <- dd_diag_narr %>%
-  filter(DIAGNOSISDATE >= start_date & DIAGNOSISDATE <= end_date & (DESCRIPTION == "" | is.na(DESCRIPTION))) %>%  
-  select(DIAGNOSIS, DESCRIPTION) %>%
+  filter(DIAGNOSISDATE >= start_date & DIAGNOSISDATE <= end_date & (Narrative == "" | is.na(Narrative))) %>%  
+  select(DIAGNOSIS, Narrative) %>%
   group_by(DIAGNOSIS) %>%
   summarise(
-    total=sum(is.na(DESCRIPTION))
+    total=sum(is.na(Narrative))
   )
  
 # determine if diagnosis is in current version of file with narratives or not
