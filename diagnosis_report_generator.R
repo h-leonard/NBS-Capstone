bdd45579b66519c2e1f10c7ae7e405447bbf1d9b
Chris Patrick <ellemsee@gmail.com>
2017-03-04 13:30:44 -0500

    Update diagnosis_report_generator.R
    Now sources load_packages and diagnosis_prep files. Changed code for diagnosis data to reflect changes to dataframe. Also allows for not running reports if run_pdfs variable exists (which is set in the summary generator file, allows that to source this file without running reports).

--- diagnosis_report_generator.R
+++ diagnosis_report_generator.R
@@ -1,24 +1,17 @@
-# Stop running code if duplicate submitter IDs have been discovered in 'main_report_generator'
-if(nrow(ID_test) != 0) {stopQuietly()}
-
-# read in descriptions of diagnoses for use in report
-diag_desc <- read.csv(paste(codes_path, slash, "diagnosis_descriptions.csv", sep=""), stringsAsFactors = FALSE)
-diag_desc <- diag_desc[,2:3]
-
-# read in sample data and reformat DIAGNOSISDATE as dates
-initial_dd_diag <- read_data(diag_data_path, "DIAGNOSISDATE")
-
-# filter diagnoses based on start date and end date
-temp_dd_diag <- create_filt_dfs(initial_dd_diag, type="diagnosis")
-dd_diag <- as.data.frame(temp_dd_diag[1])
-
+# Source loadpackages file
+load_packages <- paste0(wd, slash, "load_packages_and_functions.R")
+source(load_packages)
+ 
+# Source diagnosis_prep.R for initial preparation of dataset
+source(paste0(wd, slash, "diagnosis_prep.R"))
+ 
 # filter out diagnoses not associated with hospitals
-dd_diag_hosp <- dd_diag[dd_diag$SUBMITTERID %in% submitters$SUBMITTERID,]
-
+dd_diag_hosp <- dd_diag_narr[dd_diag_narr$SUBMITTERID %in% submitters$SUBMITTERID,]
+ 
 # add submitter name to each record
 dd_diag_hosp <- left_join(dd_diag_hosp, submitters, by="SUBMITTERID")
-names(dd_diag_hosp)[6] <- "SUBMITTERNAME"
-
+colnames(dd_diag_hosp)[which(names(dd_diag_hosp) == "HOSPITALREPORT")] <- "SUBMITTERNAME"
+ 
 # Select columns from dd_diag_hosp for report, get count of each diagnosis
 # NOTE: if a diagnosis is associated with more than one hospital (e.g., 
 # because the infant was born at a particular hospital, transferred
@@ -29,38 +22,44 @@ names(dd_diag_hosp)[6] <- "SUBMITTERNAME"
 # to count the total number of diagnoses, as this method will count some
 # diagnoses more than once.
 diagnoses_temp <- dd_diag_hosp %>%
-  select(DIAGNOSIS, SUBMITTERNAME, PATIENTID) %>%
-  group_by(SUBMITTERNAME, DIAGNOSIS, PATIENTID) %>%
+  select(DISORDER, SUBMITTERNAME, PATIENTID, Narrative) %>%
+  group_by(SUBMITTERNAME, DISORDER, PATIENTID, Narrative) %>%
   summarise()
-
+ 
 # Get count of separate diagnoses associated with samples for each hospital
 diagnoses <- diagnoses_temp %>%
-  group_by(SUBMITTERNAME, DIAGNOSIS) %>%
+  group_by(SUBMITTERNAME, DISORDER, Narrative) %>%
   summarise(Count=n())
-
+ 
+# Rearrange columns
+diagnoses <- diagnoses[,c(1:2,4,3)]
+ 
 #######################################################
-
-# Change diagnosis report to include only a single submitter (if we are only testing the 
-# functionality rather than running all reports)
-if (test_report == "Y") diagnoses = diagnoses[diagnoses$SUBMITTERNAME == diagnoses$SUBMITTERNAME[1],]
-
-# Change diagnosis report to include only the submitters indicated if only_run is not ""
-if (!is.null(only_run)) diagnoses = filter(diagnoses, SUBMITTERNAME %in% only_run)
-
-# Get date range for report
-dates <- paste(gsub(" 0", " ", format(start_date, format="%B %d, %Y")), " - ", 
-                  gsub(" 0", " ", format(end_date, format="%B %d, %Y")), sep="")
-
-# Add diagnoses descriptions to data frame
-diag_info <- inner_join(diagnoses, diag_desc, by="DIAGNOSIS")
-
-# Generate report for each hospital
-render_file_diag <- paste(wd, slash, "diagnosis_markdown.Rmd", sep="")
-
-for (submitter in diagnoses$SUBMITTERNAME){
-  rmarkdown::render(input = render_file_diag, 
-                    output_format = "pdf_document",
-                    output_file = paste(submitter, "_Diagnoses_", 
-                                        start_date, "_", end_date, ".pdf", sep=''),
-                    output_dir = hospital_path)
+ 
+# Run reports if not sourcing this file from summary_report_generator (which
+# sets the value of run_pdfs)
+if (!exists("run_pdfs")) {
+ 
+  # Change diagnosis report to include only a single submitter (if we are only testing the 
+  # functionality rather than running all reports)
+  if (test_report == "Y") diagnoses = diagnoses[diagnoses$SUBMITTERNAME == diagnoses$SUBMITTERNAME[1],]
+  
+  # Change diagnosis report to include only the submitters indicated if only_run is not ""
+  if (!is.null(only_run)) diagnoses = filter(diagnoses, SUBMITTERNAME %in% only_run)
+  
+  # Get date range for report
+  dates <- paste(gsub(" 0", " ", format(start_date, format="%B %d, %Y")), " - ", 
+                    gsub(" 0", " ", format(end_date, format="%B %d, %Y")), sep="")
+  
+  # Generate report for each hospital
+  render_file_diag <- paste(wd, slash, "diagnosis_markdown.Rmd", sep="")
+  
+  for (submitter in diagnoses$SUBMITTERNAME){
+    rmarkdown::render(input = render_file_diag, 
+                      output_format = "pdf_document",
+                      output_file = paste(submitter, "_Diagnoses_", 
+                                          start_date, "_", end_date, ".pdf", sep=''),
+                      output_dir = hospital_path)
+  }
+  
 }
