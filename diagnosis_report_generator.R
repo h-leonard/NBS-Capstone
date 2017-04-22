7305c7ef8bee3f2536a3e3a949fb2d707186ecfe
Chris Patrick <ellemsee@gmail.com>
2017-04-22 10:47:40 -0400

    Update diagnosis_report_generator.R
    Changed PATIENTID to LINKID

--- diagnosis_report_generator.R
+++ diagnosis_report_generator.R
@@ -1,17 +1,17 @@
-# Source loadpackages file
+# Source load_packages file
 load_packages <- paste0(wd, slash, "load_packages_and_functions.R")
 source(load_packages)
- 
+
 # Source diagnosis_prep.R for initial preparation of dataset
 source(paste0(wd, slash, "diagnosis_prep.R"))
- 
+
 # filter out diagnoses not associated with hospitals
 dd_diag_hosp <- dd_diag_narr[dd_diag_narr$SUBMITTERID %in% submitters$SUBMITTERID,]
- 
+
 # add submitter name to each record
 dd_diag_hosp <- left_join(dd_diag_hosp, submitters, by="SUBMITTERID")
 colnames(dd_diag_hosp)[which(names(dd_diag_hosp) == "HOSPITALREPORT")] <- "SUBMITTERNAME"
- 
+
 # Select columns from dd_diag_hosp for report, get count of each diagnosis
 # NOTE: if a diagnosis is associated with more than one hospital (e.g., 
 # because the infant was born at a particular hospital, transferred
@@ -22,20 +22,20 @@ colnames(dd_diag_hosp)[which(names(dd_diag_hosp) == "HOSPITALREPORT")] <- "SUBMI
 # to count the total number of diagnoses, as this method will count some
 # diagnoses more than once.
 diagnoses_temp <- dd_diag_hosp %>%
-  select(DISORDER, SUBMITTERNAME, PATIENTID, Narrative) %>%
-  group_by(SUBMITTERNAME, DISORDER, PATIENTID, Narrative) %>%
+  select(DISORDER, SUBMITTERNAME, LINKID, Narrative) %>%
+  group_by(SUBMITTERNAME, DISORDER, LINKID, Narrative) %>%
   summarise()
- 
+
 # Get count of separate diagnoses associated with samples for each hospital
 diagnoses <- diagnoses_temp %>%
   group_by(SUBMITTERNAME, DISORDER, Narrative) %>%
   summarise(Count=n())
- 
+
 # Rearrange columns
 diagnoses <- diagnoses[,c(1:2,4,3)]
- 
+
 #######################################################
- 
+
 # Run reports if not sourcing this file from summary_report_generator (which
 # sets the value of run_pdfs)
 if (!exists("run_pdfs")) {
